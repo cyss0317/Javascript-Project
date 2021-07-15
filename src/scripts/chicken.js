0fbdef729de5d019048b5ada547ce9f5e2d48ff9
Yun Sung Choi <cyss0317@gmail.com>
2021-07-14 22:02:49 -0500

    Added chicken_back.png

--- src/scripts/chicken.js
+++ src/scripts/chicken.js
@@ -2,7 +2,7 @@ const Util = require("./util.js");
 const MovingObject = require('./moving_object');
 
 function Chicken(options) {
-    this.pos = [300 ,400];
+    this.pos = [210 ,500];
     this.animal = new Image();
     this.animal.src = './img/Chicken_front.png'
     this.radius = 25;
@@ -25,7 +25,7 @@ Chicken.prototype.moves = function(move) {
         //move step forward
         this.animal.src = './img/Chicken_front.png'
     } else if ( prePos[1] < this.pos[1] ) {
-        this.animal.src = './img/Chicken_front.png'
+        this.animal.src = './img/Chicken_back.png'
     }
 }
 
